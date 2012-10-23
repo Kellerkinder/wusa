ccd6f4b211e5ecb61df8e1747448000112ca0fea
voxel01 <lukas01@gmail.com>
2012-10-24 01:05:23 +0200

    Use new Configurable

--- library/Wusa/Db.php
+++ library/Wusa/Db.php
@@ -2,7 +2,7 @@
 namespace Wusa;
 use Zend\Db\Adapter;
 
-class Db
+class Db extends Configurable
 {
     /**
      * Possible ways of handling new connections
@@ -14,11 +14,6 @@ class Db
      */
     const CONNECTION_TYPE_SLAVE = 'slave';
     const CONNECTION_TYPE_MASTER = 'master';
-    /**
-     * Ordner in denen nach der Config gesucht werden soll
-     * @var array
-     */
-    protected static $_confDirs = array();
     /**
      * Saves the opened Connections
      * @var array
@@ -44,7 +39,7 @@ class Db
         }
         $key = $type.'_'.$config;
 
-        $config = self::getDbConfig($config);
+        $config = self::getConfig($config);
         if(!array_key_exists($key,self::$_connections))
         {
             self::$_connections[$key] = Db\Connect::connect($config,$type,'');
@@ -55,79 +50,6 @@ class Db
         }
         return self::$_connections[$key];
 	}
-
-    /**
-     * Returns the Configfile from Configname
-     * @param $config
-     * @return bool|string
-     */
-    protected static function getConfigfile($config)
-    {
-        foreach(self::$_confDirs as $confdir)
-        {
-            $path = $confdir.DIRECTORY_SEPARATOR.$config.'.php';
-            if(file_exists($path))
-            {
-                return $path;
-            }
-        }
-        return false;
-    }
-
-    /**
-     * Adds a path to the list of configdirs
-     * @param $string
-     * @return bool
-     */
-    public static function addConfdir($string)
-    {
-        if(is_array($string))
-        {
-            $return = true;
-            foreach($string as $str)
-            {
-                $return = $return && self::addConfdir($str);
-            }
-            return $return;
-
-        }
-        if(is_dir($string))
-        {
-            self::$_confDirs[] = $string;
-            return true;
-        }
-        return false;
-    }
-
-    /**
-     * Resets the List of Configdirs
-     */
-    public static function resetConfdirs()
-    {
-        self::$_confDirs = array();
-    }
-	/**
-	 * Gets database settings
-	 * 
-	 * @param string $appName The application name
-	 * @return array
-	 */
-	public static function getDbConfig($config = NULL)
-	{
-        $configfile = self::getConfigfile($config);
-
-        if(!$configfile){
-            Config::doLog(__METHOD__.': Configfile for '.$config.' could not be found', \Zend\Log\Logger::ERR);
-            throw new \Exception('Configfile for '.$config.' could not be found');
-        }
-        $config = include $configfile;
-        if (!is_array($config)) {
-            Config::doLog(__METHOD__.': The configuration file ' . $configfile . ' does not return array', \Zend\Log\Logger::ERR);
-            throw new \Exception('The configuration file ' . $configfile . ' does not return array');
-        }
-        return new \Zend\Config\Config($config);
-	}
-
     /**
      * Holds the Active DB Connection
      * @var null|\Zend\Db\Adapter\Adapter
@@ -162,11 +84,28 @@ class Db
     /**
      * @param \Zend\Db\Sql\AbstractSql $sql
      * @param string $modeOrOptions
-     * @return mixed
+     * @return \Zend\Db\ResultSet\ResultSet
      */
     public function query(\Zend\Db\Sql\AbstractSql $sql,$modeOrOptions = \Zend\Db\Adapter\Adapter::QUERY_MODE_EXECUTE)
     {
         $string = $this->getSql()->getSqlStringForSqlObject($sql);
         return $this->connection->query($string,$modeOrOptions);
+    }
+
+    /**
+     * return the Adapter
+     * @return \Zend\Db\Adapter\Adapter
+     */
+    public function getAdapter()
+    {
+        return $this->connection;
+    }
+
+    public function insert($table, $data)
+    {
+        $sql = $this->getSql();
+        $insert = $sql->insert($table);
+        $insert->values($data);
+        return $this->query($insert);
     }
 }
