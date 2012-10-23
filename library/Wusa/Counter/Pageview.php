cbb8753a5f79b5767738532ef861cbc36b95f6bd
voxel01 <lukas01@gmail.com>
2012-10-24 01:06:45 +0200

    1st working Counter, file missed

---
+++ library/Wusa/Counter/Pageview.php
@@ -0,0 +1,54 @@
+<?php
+namespace Wusa\Counter;
+use Wusa\Config;
+use Wusa\Counter;
+/**
+ * Counting a Pageview
+ * @author lukas.plattner
+ *
+ */
+class Pageview extends Counter
+{
+    /**
+     * @see Wusa\Counter::data
+     * @var array
+     */
+    protected $data = array(
+                'uc' => '',
+                'pageId' => 0,
+                'counterId' => 1,
+                'uzeitpunkt' => NULL,
+                'szeitpunkt' => 0,
+                'referer' => NULL,
+                'viewport' => NULL,
+                'visitsession' => '',
+                );
+    /**
+     * @see Wusa\Counter::mapping
+     * @var array
+     */
+    protected $mapping = array(
+            '_wuut'	=>'uzeitpunkt',
+            '_wudr'	=>'referer',
+            '_wuvp'	=>'viewport',
+            '_wuvid'=>'visitsession',
+            );
+    
+    protected $table = 'cp_pageview';
+    public function __construct()
+    {
+        $this->table= Config::getInstance()->counter->db->prefix.'pageview';
+        $this->data['uc'] = $this->getUniqeClient();
+        $this->data['pageId'] = $this->getPageId();
+        $this->data['szeitpunkt'] = date('Y-m-d H:i:s');
+    }
+    
+    protected function refactorDataForSave($data)
+    {
+        $data['uzeitpunkt'] = date('Y-m-d H:i:s',$data['uzeitpunkt']/1000);
+        $data['visitsession'] = explode('-', $data['visitsession']);
+        $data['visitsession'] = $data['visitsession'][2].'-'.$data['visitsession'][3];
+        return $data;
+    }
+}
+?>
