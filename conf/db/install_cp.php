0cbd08025dd4340763fa7b607fc861536be03a95
voxel01 <lukas01@gmx.at>
2012-11-01 20:53:11 +0100

    DB Configs for installation

---
+++ conf/db/install_cp.php
@@ -0,0 +1,16 @@
+<?php
+$config = array();
+$config['connectionType'] = 'Single';
+
+//$config['driver'] = 'Mysqli';
+//$config['prefix'] = '';
+
+
+$config['host'] = "127.0.0.1";
+$config['port'] = "3306";
+$config['dbname'] = 'cp_cp';
+$config['username'] = 'wusatest';
+$config['password'] = 'wusatest';
+//$config['characterset'] = 'utf8';
+
+return $config;
